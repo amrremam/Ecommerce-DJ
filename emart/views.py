53d3f54ab01050235b978bba28ce8452b4e6d37c
amrremam <erorat10@gmail.com>
2023-12-05 19:26:04 +0200

    add models

--- emart/views.py
+++ emart/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProductSerializer
+
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def get_all_products(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response({"products": serializer.data})
