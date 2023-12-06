321c45ed6a0b0a188f65a7124a0fef19ad9e8d8e
amrremam <erorat10@gmail.com>
2023-12-06 16:46:44 +0200

    add pagination

--- emart/views.py
+++ emart/views.py
@@ -4,14 +4,18 @@ from rest_framework.response import Response
 from .serializers import ProductSerializer
 from django.shortcuts import render, get_object_or_404
 from .filters import ProductFilter
+from rest_framework.pagination import PageNumberPagination
 
 
 @api_view(['GET'])
 def get_all_products(request):
-    # products = Product.objects.all()
-    filterset = ProductFilter(request.GET, queryset=Product.objects.all().order_by('id'))
-    serializer = ProductSerializer(filterset.qs, many=True)
-    return Response({"products": serializer.data})
+    filter_set = ProductFilter(request.GET, queryset=Product.objects.all().order_by('id'))
+    count = filter_set.qs.count()
+    page_size = 2
+    paginator = PageNumberPagination()
+    paginator.page_size = page_size
+    serializer = ProductSerializer(filter_set.qs, many=True)
+    return Response({"products": serializer.data, "per page": page_size, "count": count})
 
 
 @api_view(['GET'])
